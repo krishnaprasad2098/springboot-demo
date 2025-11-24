71633b4560f38143a8784c748e10b7d477bca27b
krishnaprasad2098 <payamma.krishna@gmail.com>
2025-11-24 17:25:05 +0530

    infra test

--- src/main/java/com/employee_management/EmployeeManagementApplication.java
+++ src/main/java/com/employee_management/EmployeeManagementApplication.java
@@ -5,17 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-
 @SpringBootApplication(scanBasePackages = "com.employee_management")
 public class EmployeeManagementApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeManagementApplication.class, args);
-	}
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-	        return builder.sources(EmployeeManagementApplication.class);
-	    }
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EmployeeManagementApplication.class);
+    }
 
 }
